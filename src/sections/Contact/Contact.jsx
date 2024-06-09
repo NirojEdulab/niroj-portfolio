a436e11430439dac9d2a00535964eb7e3811350e
NirojEdulab <niroj@edulab.in>
2024-06-09 21:26:40 +0530

    contact form mail sending done

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,27 +1,89 @@
-import React from 'react'
-import styles from './Contact.module.css';
+import { useState } from "react";
+import styles from "./Contact.module.css";
+import Swal from 'sweetalert2'
+const web3form_secret_key = import.meta.env.VITE_WEB3FORM_KEY;
 
 const Contact = () => {
-  return (
-    <section id='contact' className={styles.container}>
-        <h1 className='sectionTitle'>Contact</h1>
-        <form action="">
-            <div className='formGroup'>
-                <label htmlFor="name" hidden>Name</label>
-                <input type="text" name="name" id="name" placeholder='Name' required />
-            </div>
-            <div className='formGroup'>
-                <label htmlFor="email" hidden>Name</label>
-                <input type="email" name="email" id="email" placeholder='Email' required />
-            </div>
-            <div className='formGroup'>
-                <label htmlFor="message" hidden>Name</label>
-                <textarea name="message" id="message" placeholder='Message' required></textarea>
-            </div>
-            <input className='btn hover' type="submit" value="Submit" />
-        </form>
-    </section>
-  )
-}
 
-export default Contact
+    const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", web3form_secret_key);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+        Swal.fire({
+            title: "Success!",
+            text: "Message sent successfully!",
+            icon: "success"
+        });
+      event.target.reset();
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+        });
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <section id="contact" className={styles.container}>
+      <h1 className="sectionTitle">Contact</h1>
+      <form onSubmit={onSubmit}>
+        <div className="formGroup">
+          <label htmlFor="name" hidden>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            Name
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="message" hidden>
+            Name
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </div>
+        {/* <input className="btn hover" type="submit" value="Submit" /> */}
+        <button className="btn hover" type="submit" disabled={isLoading}>
+          {isLoading ? "Submitting..." : "Submit"}
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
